9af78706929ee7bfacee2cf7fc7fb318753af8a6
Helanka Singhapurage <helankas26@gmail.com>
2024-02-09 22:24:53 +0530

    add MainHeader component

---
+++ src/components/core/MainHeader.tsx
@@ -0,0 +1,35 @@
+import React from "react";
+import LoggedMenu from "../shared/LoggedMenu.tsx";
+import SearchBar from "./SearchBar.tsx";
+
+const MainHeader: React.FC = () => {
+    return (
+        <>
+            <div className="bg-sky-900 fixed inset-x-0 top-1.5 mx-1.5 rounded-lg text-white p-3">
+                <nav className="nav-bar flex items-center justify-between space-x-2">
+                    <div className="font-bold cursor-pointer">
+                        <div
+                            className="text-3xl text-center bg-clip-text text-transparent bg-gradient-to-r from-rose-600 to-teal-300">
+                            Library
+                        </div>
+                    </div>
+
+                    <div className="w-1/2">
+                        <SearchBar></SearchBar>
+                    </div>
+
+                    <div className="flex items-center space-x-4">
+                        <button type="button"
+                                className="text-white bg-lime-500 hover:bg-lime-600 font-medium rounded-lg shadow-lg text-sm px-4 py-2 text-center">
+                            Login
+                        </button>
+
+                        <LoggedMenu></LoggedMenu>
+                    </div>
+                </nav>
+            </div>
+        </>
+    );
+}
+
+export default MainHeader;
