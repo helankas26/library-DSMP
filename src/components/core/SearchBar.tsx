7cd9020ee13c1dc284e1b8f81374d69110377be4
Helanka Singhapurage <helankas26@gmail.com>
2024-02-09 22:18:51 +0530

    add SearchBar component

---
+++ src/components/core/SearchBar.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+import {MagnifyingGlassIcon} from '@heroicons/react/20/solid'
+
+const SearchBar: React.FC = () => {
+    return (
+        <>
+            <div className="w-full h-9 rounded-lg flex items-center justify-center">
+                <input className="w-full h-full outline-none px-2.5 rounded-bl-2xl rounded-tl-2xl text-black"
+                       placeholder="Search books" type="search"/>
+
+                <div
+                    className="w-14 h-full rounded-br-2xl rounded-tr-2xl bg-amber-600 flex items-center justify-center hover:bg-amber-700 hover:cursor-pointer">
+                    <MagnifyingGlassIcon
+                        className="m-auto h-5 w-5 text-white"
+                        aria-hidden="true"
+                    />
+                </div>
+            </div>
+        </>
+    );
+}
+
+export default SearchBar;
