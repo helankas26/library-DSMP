3b056c4020bdb1aba6aaf37599dc6f13084ce51a
Helanka Singhapurage <helankas26@gmail.com>
2024-02-10 10:52:16 +0530

    update MainHeader component

--- src/components/core/MainHeader.tsx
+++ src/components/core/MainHeader.tsx
@@ -5,28 +5,30 @@ import SearchBar from "./SearchBar.tsx";
 const MainHeader: React.FC = () => {
     return (
         <>
-            <div className="bg-sky-900 fixed inset-x-0 top-1.5 mx-1.5 rounded-lg text-white p-3">
-                <nav className="nav-bar flex items-center justify-between space-x-2">
-                    <div className="font-bold cursor-pointer">
-                        <div
-                            className="text-3xl text-center bg-clip-text text-transparent bg-gradient-to-r from-rose-600 to-teal-300">
-                            Library
+            <div className="z-50 bg-white fixed inset-x-0 top-0 pt-2">
+                <div className="bg-sky-900 top-1.5 mx-1.5 rounded-lg text-white p-3">
+                    <nav className="nav-bar flex items-center justify-between space-x-2">
+                        <div className="font-bold cursor-pointer">
+                            <div
+                                className="text-3xl text-center bg-clip-text text-transparent bg-gradient-to-r from-rose-600 to-teal-300">
+                                Library
+                            </div>
                         </div>
-                    </div>
 
-                    <div className="w-1/2">
-                        <SearchBar></SearchBar>
-                    </div>
+                        <div className="w-1/2">
+                            <SearchBar></SearchBar>
+                        </div>
 
-                    <div className="flex items-center space-x-4">
-                        <button type="button"
-                                className="text-white bg-lime-500 hover:bg-lime-600 font-medium rounded-lg shadow-lg text-sm px-4 py-2 text-center">
-                            Login
-                        </button>
+                        <div className="flex items-center space-x-4">
+                            <button type="button"
+                                    className="text-white bg-lime-500 hover:bg-lime-600 font-medium rounded-lg shadow-lg text-sm px-4 py-2 text-center">
+                                Login
+                            </button>
 
-                        <LoggedMenu></LoggedMenu>
-                    </div>
-                </nav>
+                            <LoggedMenu></LoggedMenu>
+                        </div>
+                    </nav>
+                </div>
             </div>
         </>
     );
