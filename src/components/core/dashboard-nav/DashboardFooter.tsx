f5925605bd4709ddcac5c71c55014c2540f2bb95
Helanka Singhapurage <helankas26@gmail.com>
2024-02-13 04:57:40 +0530

    implement dashboard ui

---
+++ src/components/core/dashboard-nav/DashboardFooter.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+const DashboardFooter: React.FC = () => {
+    const currentYear = new Date().getFullYear();
+
+    return (
+        <div className="px-4 pb-1 space-y-8 overflow-hidden sm:px-6 lg:px-8 bg-white">
+            <p className=" text-base leading-6 text-center text-[#334155]">
+                Copyright &copy; {currentYear}
+                &nbsp;
+                <span
+                    className="font-bold text-xl text-center bg-clip-text text-transparent bg-gradient-to-r from-rose-600 to-teal-300">
+                        Library
+                    </span>
+                &nbsp;
+                by Helanka. All rights reserved.
+            </p>
+        </div>
+    );
+}
+
+export default DashboardFooter;
--- src/layouts/Dashboard.tsx
+++ src/layouts/Dashboard.tsx
@@ -1,9 +1,28 @@
 import React from "react";
+import DashboardNavBar from "../components/core/dashboard-nav/DashboardNavBar.tsx";
+import DashboardHeader from "../components/core/dashboard-nav/DashboardHeader.tsx";
+import DashboardFooter from "../components/core/dashboard-nav/DashboardFooter.tsx";
+import {Outlet} from "react-router-dom";
 
 const DashboardLayout: React.FC = () => {
     return (
-        <>
-        </>
+        <div className="flex flex-col bg-[#E2E8F0] h-screen">
+            <div className="flex flex-1 overflow-auto">
+                <div className="hidden sm:block sm:sticky sm:top-2 mt-2 overflow-auto no-scrollbar">
+                    <div className="pb-6">
+                        <DashboardNavBar/>
+                    </div>
+                </div>
+                <div className="w-full flex flex-col flex-1">
+                    <div className="z-50 sm:sticky sm:top-0">
+                        <DashboardHeader/>
+                    </div>
+                    <Outlet/>
+                </div>
+            </div>
+            <DashboardFooter/>
+        </div>
+
     );
 }
 
