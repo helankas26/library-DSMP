85bfa50111448a6c77e37b937a82d9f741d7b84d
Helanka Singhapurage <helankas26@gmail.com>
2024-02-14 14:50:48 +0530

    add author list view

--- src/pages/Author.tsx
+++ src/pages/Author.tsx
@@ -1,8 +1,17 @@
 import React from "react";
+import TitleBar from "../components/shared/TitleBar.tsx";
+import DashboardContext from "../components/shared/DashboardContext.tsx";
+import AuthorList from "../components/author/AuthorList.tsx";
 
 const Author: React.FC = () => {
     return (
         <>
+            <div className="sticky top-0">
+                <TitleBar title={"Authors"}/>
+            </div>
+            <DashboardContext>
+                <AuthorList/>
+            </DashboardContext>
         </>
     );
 }
