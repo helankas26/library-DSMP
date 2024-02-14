0b4e2ada64177624f6e8839c67eb310020253ed1
Helanka Singhapurage <helankas26@gmail.com>
2024-02-14 16:00:40 +0530

    add Category list view

--- src/pages/Category.tsx
+++ src/pages/Category.tsx
@@ -1,8 +1,17 @@
 import React from "react";
+import TitleBar from "../components/shared/TitleBar.tsx";
+import DashboardContext from "../components/shared/DashboardContext.tsx";
+import CategoryList from "../components/category/CategoryList.tsx";
 
 const Category: React.FC = () => {
     return (
         <>
+            <div className="sticky top-0">
+                <TitleBar title={"Categories"}/>
+            </div>
+            <DashboardContext>
+                <CategoryList/>
+            </DashboardContext>
         </>
     );
 }
