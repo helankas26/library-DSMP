980f655cc17df5ccb129d42a762be20c3aec1747
Helanka Singhapurage <helankas26@gmail.com>
2024-02-14 16:37:32 +0530

    add fine list view

--- src/pages/Fine.tsx
+++ src/pages/Fine.tsx
@@ -1,8 +1,17 @@
 import React from "react";
+import TitleBar from "../components/shared/TitleBar.tsx";
+import DashboardContext from "../components/shared/DashboardContext.tsx";
+import FineList from "../components/fine/FineList.tsx";
 
 const Fine: React.FC = () => {
     return (
         <>
+            <div className="sticky top-0">
+                <TitleBar title={"Fines"}/>
+            </div>
+            <DashboardContext>
+                <FineList/>
+            </DashboardContext>
         </>
     );
 }
