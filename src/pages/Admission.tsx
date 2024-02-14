59ac5e3d28d21e41c59e731a5d799b049f8822aa
Helanka Singhapurage <helankas26@gmail.com>
2024-02-14 16:29:19 +0530

    add Admission list view

--- src/pages/Admission.tsx
+++ src/pages/Admission.tsx
@@ -1,8 +1,17 @@
 import React from "react";
+import TitleBar from "../components/shared/TitleBar.tsx";
+import DashboardContext from "../components/shared/DashboardContext.tsx";
+import AdmissionList from "../components/admission/AdmissionList.tsx";
 
 const Admission: React.FC = () => {
     return (
         <>
+            <div className="sticky top-0">
+                <TitleBar title={"Admissions"}/>
+            </div>
+            <DashboardContext>
+                <AdmissionList/>
+            </DashboardContext>
         </>
     );
 }
