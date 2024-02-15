3adc04ac859d8030271ce8fb6cfd0b18da50b56d
Helanka Singhapurage <helankas26@gmail.com>
2024-02-15 11:19:02 +0530

    add profile list view

--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -1,8 +1,17 @@
 import React from "react";
+import TitleBar from "../components/shared/TitleBar.tsx";
+import DashboardContext from "../components/shared/DashboardContext.tsx";
+import ProfileList from "../components/profile/ProfileList.tsx";
 
 const Profile: React.FC = () => {
     return (
         <>
+            <div className="sticky top-0">
+                <TitleBar title={"Profiles"}/>
+            </div>
+            <DashboardContext>
+                <ProfileList/>
+            </DashboardContext>
         </>
     );
 }
