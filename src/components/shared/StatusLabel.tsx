e7611e71b5a1c580145084d31628334470ae1fd3
Helanka Singhapurage <helankas26@gmail.com>
2024-02-15 09:20:46 +0530

    add Status Label component

---
+++ src/components/shared/StatusLabel.tsx
@@ -0,0 +1,35 @@
+import React from "react";
+
+const StatusLabel: React.FC<{ status: string }> = (props) => {
+
+    let labelColor: string = '';
+
+    switch (props.status) {
+        case 'BORROWED':
+            labelColor = 'text-green-500 bg-green-100';
+            break;
+        case 'RESERVED':
+        case 'RETURNED':
+            labelColor = 'text-blue-500 bg-blue-100';
+            break;
+        case 'CANCELLED':
+        case 'OVERDUE':
+            labelColor = 'text-red-500 bg-red-100';
+            break;
+        case 'EXPIRED':
+            labelColor = 'text-orange-500 bg-orange-100';
+            break;
+        default:
+            labelColor = 'text-gray-900 bg-gray-100';
+            break;
+    }
+
+    return (
+        <span
+            className={`text-center align-baseline inline-flex px-2.5 py-1.5 mr-auto items-center font-semibold leading-none rounded-full ${labelColor}`}>
+            {props.status}
+        </span>
+    );
+}
+
+export default StatusLabel;
