8ec38417ea8b4d16037061759cc05e450d6a9388
Helanka Singhapurage <helankas26@gmail.com>
2024-02-15 09:44:55 +0530

    add reservation list view

--- src/pages/Reservation.tsx
+++ src/pages/Reservation.tsx
@@ -1,8 +1,17 @@
 import React from "react";
+import TitleBar from "../components/shared/TitleBar.tsx";
+import DashboardContext from "../components/shared/DashboardContext.tsx";
+import ReservationList from "../components/reservation/ReservationList.tsx";
 
 const Reservation: React.FC = () => {
     return (
         <>
+            <div className="sticky top-0">
+                <TitleBar title={"Reservations"}/>
+            </div>
+            <DashboardContext>
+                <ReservationList/>
+            </DashboardContext>
         </>
     );
 }
