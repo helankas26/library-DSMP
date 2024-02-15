e7285c5bc07b457ca9ec2453ba85671b4b50d7de
Helanka Singhapurage <helankas26@gmail.com>
2024-02-15 11:55:44 +0530

    add transaction(payment) list view

--- src/pages/Transaction.tsx
+++ src/pages/Transaction.tsx
@@ -1,8 +1,17 @@
 import React from "react";
+import TitleBar from "../components/shared/TitleBar.tsx";
+import DashboardContext from "../components/shared/DashboardContext.tsx";
+import TransactionList from "../components/transaction/TransactionList.tsx";
 
 const Transaction: React.FC = () => {
     return (
         <>
+            <div className="sticky top-0">
+                <TitleBar title={"Transaction"}/>
+            </div>
+            <DashboardContext>
+                <TransactionList/>
+            </DashboardContext>
         </>
     );
 }
