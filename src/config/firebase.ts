8079af7dedc6d0560eb84ed2c87efd15e08c3a4b
Helanka Singhapurage <helankas26@gmail.com>
2024-03-09 12:45:47 +0530

    add firebase config

---
+++ src/config/firebase.ts
@@ -0,0 +1,22 @@
+import firebase from "firebase/compat/app";
+import 'firebase/compat/auth';
+import 'firebase/compat/firestore';
+import 'firebase/compat/storage'
+
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBTOC_rCyFd02nPQk7upht7t5scXTINk1I",
+    authDomain: "library-dsmp.firebaseapp.com",
+    projectId: "library-dsmp",
+    storageBucket: "library-dsmp.appspot.com",
+    messagingSenderId: "1011905466181",
+    appId: "1:1011905466181:web:b07ffc226d0845f092f82c",
+    measurementId: "G-8J5L0SX5QF"
+};
+
+if (!firebase.apps.length) {
+    firebase.initializeApp(firebaseConfig)
+}
+
+export const storage = firebase.storage;
+export default firebase;
