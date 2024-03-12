b099f28dc0e8096577cc0491af2d889bb9b494bf
Helanka Singhapurage <helankas26@gmail.com>
2024-03-12 16:31:11 +0530

    add useSnackbar to show errors in ResetPassword & SignUp

--- src/pages/security/ResetPassword.tsx
+++ src/pages/security/ResetPassword.tsx
@@ -1,11 +1,10 @@
 import React, {FormEvent, useCallback, useEffect, useState} from "react";
-import AuthCard from "../../components/shared/AuthCard.tsx";
 import {Link} from "react-router-dom";
+import AuthCard from "../../components/shared/AuthCard.tsx";
 import resetPasswordImage from "../../assets/reset-password.jpg";
-import SnackbarServiceProps from "../../model/SnackbarServiceProps.ts";
 import authService from "../../services/api/auth.ts";
-import SnackbarService from "../../components/shared/SnackbarService.tsx";
 import ValidationIcon from "../../components/shared/ValidationIcon.tsx";
+import useSnackbar from "../../hooks/use-snackbar.ts";
 
 const ResetPassword: React.FC = () => {
     const [otp, setOtp] = useState<string>('');
@@ -14,7 +13,7 @@ const ResetPassword: React.FC = () => {
     const [isOtpValid, setIsOtpValid] = useState<boolean>();
     const [loading, setLoading] = useState<boolean>(false);
     const [isSignUpDisabled, setIsSignUpDisabled] = useState<boolean>(true);
-    const [error, setError] = useState<SnackbarServiceProps>();
+    const {showError} = useSnackbar();
 
     const checkOtpValidHandler = useCallback(async () => {
         if (otp.length === 16) {
@@ -24,14 +23,7 @@ const ResetPassword: React.FC = () => {
                 setIsOtpValid(response.data.valid);
                 setIsSignUpDisabled(!response.data.valid);
             } catch (error: any) {
-                setError({
-                    message: error.response.data.message,
-                    isOpen: true,
-                    severity: 'error',
-                    onClear: () => {
-                        setError(undefined);
-                    }
-                });
+                showError(error);
             } finally {
                 setLoading(false);
             }
@@ -57,90 +49,87 @@ const ResetPassword: React.FC = () => {
     };
 
     return (
-        <>
-            {error && <SnackbarService options={error}/>}
-            <AuthCard imageUrl={resetPasswordImage}>
-                <div className="px-8 mb-4 text-center">
-                    <h2 className="mt-4 text-3xl font-bold text-gray-900">
-                        Reset Your Password?
-                    </h2>
-                    <p className="mt-2 text-sm text-gray-600">
-                        Reset your account password using OTP, check your email inbox or spam folder for OTP.
-                    </p>
+        <AuthCard imageUrl={resetPasswordImage}>
+            <div className="px-8 mb-4 text-center">
+                <h2 className="mt-4 text-3xl font-bold text-gray-900">
+                    Reset Your Password?
+                </h2>
+                <p className="mt-2 text-sm text-gray-600">
+                    Reset your account password using OTP, check your email inbox or spam folder for OTP.
+                </p>
+            </div>
+            <form className="px-8 pt-6 pb-8 mb-4 bg-white rounded" onSubmit={resetPasswordHandler}>
+                <div className="relative mb-6">
+                    <div className="absolute right-0 mt-7">
+                        <ValidationIcon isValid={isOtpValid} loading={loading}/>
+                    </div>
+                    <label className="block mb-1 text-sm font-bold text-gray-700 tracking-wide" htmlFor="otp">
+                        OTP
+                    </label>
+                    <input
+                        className="w-full py-2 text-sm leading-tight text-gray-700 border-b border-gray-300 appearance-none focus:outline-none focus:border-indigo-500"
+                        id="otp"
+                        type="text"
+                        value={otp}
+                        onChange={(e) => {
+                            setOtp(e.target.value);
+                        }}
+                        placeholder="Enter OTP"/>
                 </div>
-                <form className="px-8 pt-6 pb-8 mb-4 bg-white rounded" onSubmit={resetPasswordHandler}>
-                    <div className="relative mb-6">
-                        <div className="absolute right-0 mt-7">
-                            <ValidationIcon isValid={isOtpValid} loading={loading}/>
-                        </div>
-                        <label className="block mb-1 text-sm font-bold text-gray-700 tracking-wide" htmlFor="otp">
-                            OTP
+                <div className="mb-4 md:flex md:justify-between">
+                    <div className="mb-4 md:mr-2 md:mb-0">
+                        <label className="block mb-2 text-sm font-bold text-gray-700 tracking-wide"
+                               htmlFor="password">
+                            Password
                         </label>
                         <input
-                            className="w-full py-2 text-sm leading-tight text-gray-700 border-b border-gray-300 appearance-none focus:outline-none focus:border-indigo-500"
-                            id="otp"
-                            type="text"
-                            value={otp}
+                            className="w-full px-3 py-2 mb-3 text-sm leading-tight text-gray-700 border rounded shadow appearance-none focus:outline-none focus:shadow-outline"
+                            id="password"
+                            type="password"
+                            value={password}
                             onChange={(e) => {
-                                setOtp(e.target.value);
+                                setPassword(e.target.value);
                             }}
-                            placeholder="Enter OTP"/>
+                            placeholder="Enter your password"
+                        />
                     </div>
-                    <div className="mb-4 md:flex md:justify-between">
-                        <div className="mb-4 md:mr-2 md:mb-0">
-                            <label className="block mb-2 text-sm font-bold text-gray-700 tracking-wide"
-                                   htmlFor="password">
-                                Password
-                            </label>
-                            <input
-                                className="w-full px-3 py-2 mb-3 text-sm leading-tight text-gray-700 border rounded shadow appearance-none focus:outline-none focus:shadow-outline"
-                                id="password"
-                                type="password"
-                                value={password}
-                                onChange={(e) => {
-                                    setPassword(e.target.value);
-                                }}
-                                placeholder="Enter your password"
-                            />
-                        </div>
-                        <div className="md:ml-2">
-                            <label className="block mb-2 text-sm font-bold text-gray-700 tracking-wide"
-                                   htmlFor="confirmPassword">
-                                Confirm Password
-                            </label>
-                            <input
-                                className="w-full px-3 py-2 mb-3 text-sm leading-tight text-gray-700 border rounded shadow appearance-none focus:outline-none focus:shadow-outline"
-                                id="confirmPassword"
-                                type="password"
-                                value={confirmPassword}
-                                onChange={(e) => {
-                                    setConfirmPassword(e.target.value);
-                                }}
-                                placeholder="Confirm your password"
-                            />
-                        </div>
+                    <div className="md:ml-2">
+                        <label className="block mb-2 text-sm font-bold text-gray-700 tracking-wide"
+                               htmlFor="confirmPassword">
+                            Confirm Password
+                        </label>
+                        <input
+                            className="w-full px-3 py-2 mb-3 text-sm leading-tight text-gray-700 border rounded shadow appearance-none focus:outline-none focus:shadow-outline"
+                            id="confirmPassword"
+                            type="password"
+                            value={confirmPassword}
+                            onChange={(e) => {
+                                setConfirmPassword(e.target.value);
+                            }}
+                            placeholder="Confirm your password"
+                        />
                     </div>
-                    <div className="mb-6 text-center">
-                        <button
-                            className="w-full px-4 py-2 font-bold text-white bg-red-500 rounded-full hover:bg-red-700 focus:outline-none focus:shadow-outline disabled:cursor-not-allowed disabled:bg-gray-500"
-                            disabled={isSignUpDisabled}
-                            type="submit">
-                            Reset Password
-                        </button>
-                    </div>
-                    <hr className="mt-10 border"/>
-                    <div className="text-center flex flex-col items-center justify-center">
-                        <p className="mt-10 text-md text-gray-500">
-                            Already have an account?
-                        </p>
-                        <Link to="/auth/login"
-                              className="inline-block text-sm text-blue-500 align-baseline hover:text-blue-800">
-                            Log in
-                        </Link>
-                    </div>
-                </form>
-            </AuthCard>
-        </>
+                </div>
+                <div className="mb-6 text-center">
+                    <button
+                        className="w-full px-4 py-2 font-bold text-white bg-red-500 rounded-full hover:bg-red-700 focus:outline-none focus:shadow-outline disabled:cursor-not-allowed disabled:bg-gray-500"
+                        disabled={isSignUpDisabled}
+                        type="submit">
+                        Reset Password
+                    </button>
+                </div>
+                <hr className="mt-10 border"/>
+                <div className="text-center flex flex-col items-center justify-center">
+                    <p className="mt-10 text-md text-gray-500">
+                        Already have an account?
+                    </p>
+                    <Link to="/auth/login"
+                          className="inline-block text-sm text-blue-500 align-baseline hover:text-blue-800">
+                        Log in
+                    </Link>
+                </div>
+            </form>
+        </AuthCard>
     );
 }
 
--- src/pages/security/Signup.tsx
+++ src/pages/security/Signup.tsx
@@ -1,11 +1,10 @@
 import React, {FormEvent, useCallback, useEffect, useState} from "react";
-import AuthCard from "../../components/shared/AuthCard.tsx";
 import {Link} from "react-router-dom";
+import AuthCard from "../../components/shared/AuthCard.tsx";
 import signupImage from "../../assets/signup.jpg";
 import authService from "../../services/api/auth.ts";
 import ValidationIcon from "../../components/shared/ValidationIcon.tsx";
-import SnackbarService from "../../components/shared/SnackbarService.tsx";
-import SnackbarServiceProps from "../../model/SnackbarServiceProps.ts";
+import useSnackbar from "../../hooks/use-snackbar.ts";
 
 const Signup: React.FC = () => {
     const [registrationNo, setRegistrationNo] = useState<string>('');
@@ -15,7 +14,7 @@ const Signup: React.FC = () => {
     const [isRegistrationValid, setIsRegistrationValid] = useState<boolean>();
     const [loading, setLoading] = useState<boolean>(false);
     const [isSignUpDisabled, setIsSignUpDisabled] = useState<boolean>(true);
-    const [error, setError] = useState<SnackbarServiceProps>();
+    const {showError} = useSnackbar();
 
     const checkRegistrationValidHandler = useCallback(async () => {
         if (registrationNo.length === 10) {
@@ -25,14 +24,7 @@ const Signup: React.FC = () => {
                 setIsRegistrationValid(response.data.valid);
                 setIsSignUpDisabled(!response.data.valid);
             } catch (error: any) {
-                setError({
-                    message: error.response.data.message,
-                    isOpen: true,
-                    severity: 'error',
-                    onClear: () => {
-                        setError(undefined);
-                    }
-                });
+                showError(error);
             } finally {
                 setLoading(false);
             }
@@ -58,103 +50,100 @@ const Signup: React.FC = () => {
     };
 
     return (
-        <>
-            {error && <SnackbarService options={error}/>}
-            <AuthCard imageUrl={signupImage}>
-                <div className="text-center">
-                    <h2 className="mt-4 text-3xl font-bold text-gray-900">
-                        Create an Account
-                    </h2>
-                    <p className="mt-2 text-sm text-gray-600">Please sign up to your account</p>
-                </div>
-                <form className="px-8 pt-6 pb-8 mb-4 bg-white rounded" onSubmit={signupHandler}>
-                    <div className="relative mb-4">
-                        <div className="absolute right-0 mt-7">
-                            <ValidationIcon isValid={isRegistrationValid} loading={loading}/>
-                        </div>
-                        <label className="block mb-1 text-sm font-bold text-gray-700 tracking-wide" htmlFor="regNo">
-                            Registration No
-                        </label>
-                        <input
-                            className="w-full py-2 text-sm leading-tight text-gray-700 border-b border-gray-300 appearance-none focus:outline-none focus:border-indigo-500"
-                            id="regNo"
-                            type="text"
-                            value={registrationNo}
-                            onChange={(e) => {
-                                setRegistrationNo(e.target.value);
-                            }}
-                            placeholder="Enter your registration No"/>
+        <AuthCard imageUrl={signupImage}>
+            <div className="text-center">
+                <h2 className="mt-4 text-3xl font-bold text-gray-900">
+                    Create an Account
+                </h2>
+                <p className="mt-2 text-sm text-gray-600">Please sign up to your account</p>
+            </div>
+            <form className="px-8 pt-6 pb-8 mb-4 bg-white rounded" onSubmit={signupHandler}>
+                <div className="relative mb-4">
+                    <div className="absolute right-0 mt-7">
+                        <ValidationIcon isValid={isRegistrationValid} loading={loading}/>
                     </div>
-                    <div className="mb-4">
-                        <label className="block mb-2 text-sm font-bold text-gray-700 tracking-wide" htmlFor="username">
-                            Username
+                    <label className="block mb-1 text-sm font-bold text-gray-700 tracking-wide" htmlFor="regNo">
+                        Registration No
+                    </label>
+                    <input
+                        className="w-full py-2 text-sm leading-tight text-gray-700 border-b border-gray-300 appearance-none focus:outline-none focus:border-indigo-500"
+                        id="regNo"
+                        type="text"
+                        value={registrationNo}
+                        onChange={(e) => {
+                            setRegistrationNo(e.target.value);
+                        }}
+                        placeholder="Enter your registration No"/>
+                </div>
+                <div className="mb-4">
+                    <label className="block mb-2 text-sm font-bold text-gray-700 tracking-wide" htmlFor="username">
+                        Username
+                    </label>
+                    <input
+                        className="w-full px-3 py-2 mb-3 text-sm leading-tight text-gray-700 border rounded shadow appearance-none focus:outline-none focus:shadow-outline"
+                        id="username"
+                        type="text"
+                        value={username}
+                        onChange={(e) => {
+                            setUsername(e.target.value);
+                        }}
+                        placeholder="Enter your username"
+                    />
+                </div>
+                <div className="mb-4 md:flex md:justify-between">
+                    <div className="mb-4 md:mr-2 md:mb-0">
+                        <label className="block mb-2 text-sm font-bold text-gray-700 tracking-wide"
+                               htmlFor="password">
+                            Password
                         </label>
                         <input
                             className="w-full px-3 py-2 mb-3 text-sm leading-tight text-gray-700 border rounded shadow appearance-none focus:outline-none focus:shadow-outline"
-                            id="username"
-                            type="text"
-                            value={username}
+                            id="password"
+                            type="password"
+                            value={password}
                             onChange={(e) => {
-                                setUsername(e.target.value);
+                                setPassword(e.target.value);
                             }}
-                            placeholder="Enter your username"
+                            placeholder="Enter your password"
                         />
                     </div>
-                    <div className="mb-4 md:flex md:justify-between">
-                        <div className="mb-4 md:mr-2 md:mb-0">
-                            <label className="block mb-2 text-sm font-bold text-gray-700 tracking-wide"
-                                   htmlFor="password">
-                                Password
-                            </label>
-                            <input
-                                className="w-full px-3 py-2 mb-3 text-sm leading-tight text-gray-700 border rounded shadow appearance-none focus:outline-none focus:shadow-outline"
-                                id="password"
-                                type="password"
-                                value={password}
-                                onChange={(e) => {
-                                    setPassword(e.target.value);
-                                }}
-                                placeholder="Enter your password"
-                            />
-                        </div>
-                        <div className="md:ml-2">
-                            <label className="block mb-2 text-sm font-bold text-gray-700 tracking-wide"
-                                   htmlFor="confirmPassword">
-                                Confirm Password
-                            </label>
-                            <input
-                                className="w-full px-3 py-2 mb-3 text-sm leading-tight text-gray-700 border rounded shadow appearance-none focus:outline-none focus:shadow-outline"
-                                id="confirmPassword"
-                                type="password"
-                                value={confirmPassword}
-                                onChange={(e) => {
-                                    setConfirmPassword(e.target.value);
-                                }}
-                                placeholder="Confirm your password"
-                            />
-                        </div>
+                    <div className="md:ml-2">
+                        <label className="block mb-2 text-sm font-bold text-gray-700 tracking-wide"
+                               htmlFor="confirmPassword">
+                            Confirm Password
+                        </label>
+                        <input
+                            className="w-full px-3 py-2 mb-3 text-sm leading-tight text-gray-700 border rounded shadow appearance-none focus:outline-none focus:shadow-outline"
+                            id="confirmPassword"
+                            type="password"
+                            value={confirmPassword}
+                            onChange={(e) => {
+                                setConfirmPassword(e.target.value);
+                            }}
+                            placeholder="Confirm your password"
+                        />
                     </div>
-                    <div className="mb-6 text-center">
-                        <button
-                            className="w-full px-4 py-2 font-bold text-white bg-blue-500 rounded-full hover:bg-blue-700 focus:outline-none focus:shadow-outline disabled:cursor-not-allowed disabled:bg-gray-500"
-                            disabled={isSignUpDisabled}
-                            type="submit">
-                            Register Account
-                        </button>
-                    </div>
-                    <hr className="mt-10 border"/>
-                    <div className="text-center flex flex-col items-center justify-center">
-                        <p className="mt-10 text-md text-gray-500">
-                            Already have an account?
-                        </p>
-                        <Link to="/auth/login"
-                              className="inline-block text-sm text-blue-500 align-baseline hover:text-blue-800">
-                            Log in
-                        </Link>
-                    </div>
-                </form>
-            </AuthCard>
-        </>
+                </div>
+                <div className="mb-6 text-center">
+                    <button
+                        className="w-full px-4 py-2 font-bold text-white bg-blue-500 rounded-full hover:bg-blue-700 focus:outline-none focus:shadow-outline disabled:cursor-not-allowed disabled:bg-gray-500"
+                        disabled={isSignUpDisabled}
+                        type="submit">
+                        Register Account
+                    </button>
+                </div>
+                <hr className="mt-10 border"/>
+                <div className="text-center flex flex-col items-center justify-center">
+                    <p className="mt-10 text-md text-gray-500">
+                        Already have an account?
+                    </p>
+                    <Link to="/auth/login"
+                          className="inline-block text-sm text-blue-500 align-baseline hover:text-blue-800">
+                        Log in
+                    </Link>
+                </div>
+            </form>
+        </AuthCard>
     );
 }
 
