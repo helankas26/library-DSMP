53a81dac39909708370209a38f03819952239c23
Helanka Singhapurage <helankas26@gmail.com>
2024-03-17 20:49:50 +0530

    refactor AxiosInstance

--- src/config/axios-instance.ts
+++ src/config/axios-instance.ts
@@ -2,28 +2,10 @@ import axios, {AxiosInstance} from "axios";
 import BASE_URL from './api-config.ts';
 
 const instance: AxiosInstance = axios.create({
-    baseURL: BASE_URL
+    baseURL: BASE_URL,
+    headers: {"Content-Type": "application/json"},
+    responseType: "json",
+    withCredentials: true
 });
-
-/*// Add a request interceptor
-instance.interceptors.request.use(function (config) {
-    // Do something before request is sent
-    return config;
-}, function (error) {
-    // Do something with request error
-    return Promise.reject(error);
-});
-
-// Add a response interceptor
-instance.interceptors.response.use(function (response) {
-    // Any status code that lie within the range of 2xx cause this function to trigger
-    // Do something with response data
-    return response;
-}, function (error) {
-    // Any status codes that falls outside the range of 2xx cause this function to trigger
-    // Do something with response error
-    return Promise.reject(error);
-});*/
-
 
 export default instance;
