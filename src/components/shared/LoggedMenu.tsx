d2e68a3e0630a938125e85dfa02dd2e07b64ff16
Helanka Singhapurage <helankas26@gmail.com>
2024-03-20 18:43:26 +0530

    complete LoggedMenu ✅

--- src/components/shared/LoggedMenu.tsx
+++ src/components/shared/LoggedMenu.tsx
@@ -1,18 +1,34 @@
 import React from "react";
 import {Menu} from '@headlessui/react'
+import {Link, useNavigate} from "react-router-dom";
+
+import profileAvatarImage from "../../assets/profile-avatar.jpg";
+import useLogout from "../../hooks/use-logout.ts";
+import useAuth from "../../hooks/use-auth.ts";
+
 
 function classNames(...classes: string[]) {
     return classes.filter(Boolean).join(' ')
 }
 
 const LoggedMenu: React.FC = () => {
+    const navigate = useNavigate();
+    const {auth} = useAuth()!;
+    const logout = useLogout();
+
+    const signOutHandler = async () => {
+        navigate("/");
+        await logout();
+    }
+
     return (
         <>
             <Menu as="div" className="relative inline-block text-left mt-1">
                 <div className="-mb-1">
                     <Menu.Button
                         className="text-sm bg-gray-600 hover:bg-slate-700 rounded-full">
-                        <img className="w-8 h-8 rounded-full" src="" alt="user photo"/>
+                        <img className="w-8 h-8 rounded-full"
+                             src={auth.profile?.avatar ? auth.profile.avatar : profileAvatarImage} alt="user photo"/>
                     </Menu.Button>
                 </div>
 
@@ -22,8 +38,12 @@ const LoggedMenu: React.FC = () => {
                         <div className="px-4 py-3">
                             <Menu.Item>
                                 <>
-                                    <span className="block text-sm text-gray-900">Helanka S.</span>
-                                    <span className="block text-sm text-gray-500 truncate">helankas26@gmail.com</span>
+                                    <span className="block text-sm text-gray-900">
+                                        {auth.profile ? auth.profile.fullName : 'Helanka S.'}
+                                    </span>
+                                    <span className="block text-sm text-gray-500 truncate">
+                                        {auth.profile ? auth.profile.email : 'helankas26@gmail.com'}
+                                    </span>
                                 </>
                             </Menu.Item>
                         </div>
@@ -31,26 +51,28 @@ const LoggedMenu: React.FC = () => {
                         <div>
                             <Menu.Item>
                                 {({active}) => (
-                                    <a href="#"
-                                       className={classNames(active ? 'bg-gray-100 text-gray-900' : 'text-gray-700', 'block px-4 py-2 text-sm')}>
+                                    <Link
+                                        to="/dashboard"
+                                        className={classNames(active ? 'bg-gray-100 text-gray-900' : 'text-gray-700', 'block px-4 py-2 text-sm')}>
                                         Dashboard
-                                    </a>
+                                    </Link>
                                 )}
                             </Menu.Item>
                             <Menu.Item>
                                 {({active}) => (
-                                    <a
-                                        href="#"
+                                    <Link
+                                        to="/dashboard/account/settings"
                                         className={classNames(active ? 'bg-gray-100 text-gray-900' : 'text-gray-700', 'block px-4 py-2 text-sm')}>
                                         Account settings
-                                    </a>
+                                    </Link>
                                 )}
                             </Menu.Item>
                             <Menu.Item>
                                 {({active}) => (
                                     <button
-                                        type="submit"
-                                        className={classNames(active ? 'bg-gray-100 text-gray-900' : 'text-gray-700', 'block w-full px-4 py-2 text-left text-sm')}>
+                                        className={classNames(active ? 'bg-gray-100 text-gray-900' : 'text-gray-700', 'block w-full px-4 py-2 text-left text-sm')}
+                                        type="button"
+                                        onClick={signOutHandler}>
                                         Sign out
                                     </button>
                                 )}
