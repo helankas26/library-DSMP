b10a85dcbc36cbf8e3f8cbfbdd676312cb10bfd7
Helanka Singhapurage <helankas26@gmail.com>
2024-03-24 14:53:44 +0530

    refactor useLogout hook add navigate to home

--- src/hooks/use-logout.ts
+++ src/hooks/use-logout.ts
@@ -1,13 +1,17 @@
+import {useNavigate} from "react-router-dom";
+
 import useAuth from "./use-auth.ts";
 import useSnackbar from "./use-snackbar.ts";
 import authService from "../services/api/auth.ts";
 
 const useLogout = () => {
     const {dispatchAuth} = useAuth();
+    const navigate = useNavigate();
     const {showError} = useSnackbar();
 
     const logout = async () => {
         dispatchAuth({type: 'LOGOUT', auth: {accessToken: undefined, profile: undefined}});
+        navigate("/");
 
         try {
             await authService.logout();
