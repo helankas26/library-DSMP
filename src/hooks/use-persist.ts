8f3dea7936971db9b45f05718042f8f9d8c59ca5
Helanka Singhapurage <helankas26@gmail.com>
2024-03-25 08:28:29 +0530

    remove access token return in usePersist

--- src/hooks/use-persist.ts
+++ src/hooks/use-persist.ts
@@ -10,8 +10,6 @@ const usePersist = () => {
         try {
             const response = await authService.refreshToken();
             dispatchAuth({type: 'SET_TOKEN', auth: {accessToken: response.data.accessToken}});
-
-            return response.data.accessToken;
         } catch (error: any) {
             dispatchAuth({type: 'LOGOUT', auth: {accessToken: undefined, profile: undefined}});
             navigate("/");
