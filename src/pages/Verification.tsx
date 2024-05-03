1d43ead917cf254f799b7a141a2457b2997164f8
Helanka Singhapurage <helankas26@gmail.com>
2024-05-03 17:58:25 +0530

    add Verification page to verify and load dashboard routes

---
+++ src/pages/Verification.tsx
@@ -0,0 +1,52 @@
+import React, {useEffect} from "react";
+import {useNavigate} from "react-router-dom";
+
+import useAuth from "../hooks/use-auth.ts";
+import usePersist from "../hooks/use-persist.ts";
+import {useAppDispatch} from "../hooks/use-store.ts";
+import {loadDashboardRoutes} from "../store/dashboard-route/dashboard-route-actions.ts";
+
+const Verification: React.FC = () => {
+    const {auth} = useAuth();
+    const persist = usePersist();
+    const navigate = useNavigate();
+    const dispatch = useAppDispatch();
+
+    useEffect(() => {
+        const verifyRefreshToken = async () => {
+            if (!auth.accessToken) {
+                await persist();
+            }
+        }
+
+        verifyRefreshToken();
+    }, []);
+
+    useEffect(() => {
+        const getDashboardRoutes = () => {
+            return new Promise((resolve) => {
+                setTimeout(() => {
+                    resolve(dispatch(loadDashboardRoutes()));
+                }, 1000);
+            });
+        }
+
+        if (auth.accessToken && auth.profile) {
+            getDashboardRoutes().then(() => {
+                navigate('/dashboard/console', {replace: true});
+            });
+        }
+    }, [dispatch, navigate]);
+
+    return (
+        <div className="flex flex-col w-full h-screen items-center justify-center">
+            <div
+                className="p-2.5 text-6xl font-bold text-center bg-clip-text text-transparent bg-gradient-to-r from-rose-600 to-teal-300 transition-all group-hover:text-7xl">
+                Library
+            </div>
+            <p className="text-[1.17rem] text-center">loading...</p>
+        </div>
+    );
+}
+
+export default Verification;
