11d68d6b556a1e0c63718d967577fee86106f706
Helanka Singhapurage <helankas26@gmail.com>
2024-05-03 20:13:45 +0530

    bug fix in useLogout hook bug: reauthenticate when page reload. fix: call logout api to clear refresh token. and add snackbar alert

--- src/hooks/use-logout.ts
+++ src/hooks/use-logout.ts
@@ -1,17 +1,22 @@
 import {useLocation, useNavigate} from "react-router-dom";
 
 import useAuth from "./use-auth.ts";
+import authService from "../services/api/auth.ts";
 import {removeRefreshTokenExpirationDate} from "../utils/local-storage.ts";
+import useSnackbar from "./use-snackbar.ts";
 
 const useLogout = () => {
     const navigate = useNavigate();
     const location = useLocation();
     const {dispatchAuth} = useAuth();
+    const {showAlert} = useSnackbar();
 
     const logout = async () => {
+        await authService.logout();
         dispatchAuth({type: 'LOGOUT', auth: {accessToken: undefined, profile: undefined}});
         removeRefreshTokenExpirationDate();
         if (location.pathname.startsWith("/dashboard") || location.pathname.startsWith("/books")) navigate("/auth/login", {state: {from: location}})
+        showAlert("Automatically logout. Please login again!", 'warning');
     }
 
     return logout;
