c7ee8e2d98e80aae901ac95030c9e6d65f1ec47f
Helanka Singhapurage <helankas26@gmail.com>
2024-05-10 09:44:48 +0530

    add profile firebase service

--- src/config/firebase.ts
+++ src/config/firebase.ts
@@ -18,5 +18,5 @@ if (!firebase.apps.length) {
     firebase.initializeApp(firebaseConfig)
 }
 
-export const storage = firebase.storage;
+export const storage = firebase.storage();
 export default firebase;
---
+++ src/services/firebase/profile.ts
@@ -0,0 +1,22 @@
+import firebase, {storage} from "../../config/firebase";
+
+const PROFILE_IMAGES: string = 'images/profiles';
+
+const saveProfileImage = async (image: File) => {
+    const storageRef = storage.ref();
+    const imageRef = storageRef.child(`${PROFILE_IMAGES}/${Date.now() + '-' + image.name}`);
+    const snapshot = await imageRef.put(image);
+    return await snapshot.ref.getDownloadURL();
+}
+
+const deleteProfileImage = async (imageURL: string) => {
+    try {
+        const storage = firebase.storage();
+        const imageRef = storage.refFromURL(imageURL);
+        return await imageRef.delete();
+    } catch (error: any) {
+        return;
+    }
+}
+
+export default {saveProfileImage, deleteProfileImage};
