372736e0d5a50dff5359ce24b98d99f6fdec98c4
Helanka Singhapurage <helankas26@gmail.com>
2024-05-10 09:45:23 +0530

    refactor SaveRecordButton component

--- src/components/shared/SaveRecordButton.tsx
+++ src/components/shared/SaveRecordButton.tsx
@@ -1,13 +1,13 @@
 import React from "react";
 
-const SaveRecordButton: React.FC<{ model: string }> = (props) => {
-
+const SaveRecordButton: React.FC<{ model: string; isSubmitting: boolean }> = (props) => {
     return (
-
         <button
-            className="w-full py-2 px-4 font-semibold text-white rounded shadow border border-blue-500 transition duration-150 bg-blue-500 hover:bg-blue-700 active:bg-blue-500"
-            type="submit">
-            Save {props.model}
+            className="w-full py-2 px-4 font-semibold text-white rounded shadow border border-blue-500 transition duration-150 bg-blue-500 hover:bg-blue-700 active:bg-blue-500 disabled:cursor-not-allowed disabled:bg-gray-500"
+            type="submit"
+            disabled={props.isSubmitting}>
+            {!props.isSubmitting && (`Save ${props.model}`)}
+            {props.isSubmitting && 'Saving . . .'}
         </button>
     );
 }
