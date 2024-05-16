9e57b560c11eaa38f81ddcb6241a9534dbab8bd7
Helanka Singhapurage <helankas26@gmail.com>
2024-05-16 14:02:42 +0530

    reduce height of MainFooter

--- src/components/core/MainFooter.tsx
+++ src/components/core/MainFooter.tsx
@@ -5,30 +5,30 @@ const MainFooter: React.FC = () => {
 
     return (
         <div className="bg-[#E2E8F0]">
-            <div className="max-w-screen-xl px-4 py-6 mx-auto space-y-8 overflow-hidden sm:px-6 lg:px-8">
+            <div className="max-w-screen-xl px-4 pt-6 pb-2 mx-auto overflow-hidden sm:px-6 lg:px-8">
                 <nav className="flex flex-wrap justify-center -mx-5 -my-2">
-                    <div className="px-5 py-2">
+                    <div className="px-5">
                         <p className="text-base leading-6 text-gray-500 hover:text-gray-900 cursor-pointer">
                             About
                         </p>
                     </div>
-                    <div className="px-5 py-2">
+                    <div className="px-5">
                         <p className="text-base leading-6 text-gray-500 hover:text-gray-900 cursor-pointer">
                             Team
                         </p>
                     </div>
-                    <div className="px-5 py-2">
+                    <div className="px-5">
                         <p className="text-base leading-6 text-gray-500 hover:text-gray-900 cursor-pointer">
                             Contact
                         </p>
                     </div>
-                    <div className="px-5 py-2">
+                    <div className="px-5">
                         <p className="text-base leading-6 text-gray-500 hover:text-gray-900 cursor-pointer">
                             Terms
                         </p>
                     </div>
                 </nav>
-                <div className="flex justify-center mt-8 space-x-6">
+                <div className="flex justify-center mt-6 space-x-6">
                     <a href="https://web.facebook.com/helanka.singhapurage" target='_blank'
                        className="text-gray-400 hover:text-gray-500">
                         <svg className="w-6 h-6" aria-hidden="true" fill="currentColor" viewBox="0 0 24 24">
@@ -58,8 +58,8 @@ const MainFooter: React.FC = () => {
                         </svg>
                     </a>
                 </div>
-                <hr className="my-6 border-[#CBD5E1]"/>
-                <p className="mt-8 text-base leading-6 text-center text-[#334155]">
+                <hr className="mt-3 border-[#CBD5E1]"/>
+                <p className="mt-2 text-base leading-6 text-center text-[#334155]">
                     Copyright &copy; {currentYear}
                     &nbsp;
                     <span
