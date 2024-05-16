0c73a84c75d25f4bd1cb0d33a195886a237a395c
Helanka Singhapurage <helankas26@gmail.com>
2024-05-16 13:04:10 +0530

    change pt-2 to mt-2 in DashboardHeader

--- src/components/core/dashboard-nav/DashboardHeader.tsx
+++ src/components/core/dashboard-nav/DashboardHeader.tsx
@@ -8,7 +8,7 @@ const DashboardHeader: React.FC = () => {
     const subRoutes = useAppSelector((state) => state.subRoutes);
 
     return (
-        <div className="bg-[#E2E8F0] pt-2">
+        <div className="bg-[#E2E8F0] mt-2">
             <div
                 className="mx-1.5 py-2 px-2 bg-white rounded-lg flex items-center justify-between">
                 <div className="px-2 mr-2 border-r border-gray-800">
