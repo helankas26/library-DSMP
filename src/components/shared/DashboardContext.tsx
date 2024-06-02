7cf16b7494f1b84296a24222557d26982579f063
Helanka Singhapurage <helankas26@gmail.com>
2024-06-03 00:05:17 +0530

    add scroll to top in DashboardContext when change route scroll to top

--- src/components/shared/DashboardContext.tsx
+++ src/components/shared/DashboardContext.tsx
@@ -1,10 +1,20 @@
-import React, {ReactNode} from "react";
+import React, {ReactNode, useEffect} from "react";
+import {useLocation} from "react-router-dom";
+
+import useScrollToTop from "../../hooks/use-scroll-to-top.ts";
 
 const DashboardContext: React.FC<{ children: ReactNode }> = (props) => {
+    const {pathname} = useLocation();
+    const {elementRef, scrollToTop} = useScrollToTop<HTMLDivElement>();
     const {children} = props;
+
+    useEffect(() => {
+        scrollToTop();
+    }, [pathname]);
 
     return (
         <div className="bg-white p-8 w-full h-full overflow-hidden overflow-y-auto no-scrollbar">
+            <div ref={elementRef} className="invisible"></div>
             {children}
         </div>
     );
