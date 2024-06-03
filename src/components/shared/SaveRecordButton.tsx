c812a778e1f22a2b47d99b46f74d0d05c8e98e22
Helanka Singhapurage <helankas26@gmail.com>
2024-06-03 20:26:05 +0530

    remove border in SaveRecordButton

--- src/components/shared/SaveRecordButton.tsx
+++ src/components/shared/SaveRecordButton.tsx
@@ -5,7 +5,7 @@ const SaveRecordButton: React.FC<{ model: string; isSubmitting: boolean }> = (pr
 
     return (
         <button
-            className="w-full py-2 px-4 font-semibold text-white rounded shadow border border-blue-500 transition duration-150 bg-blue-500 hover:bg-blue-700 active:bg-blue-500 disabled:cursor-not-allowed disabled:bg-gray-500"
+            className="w-full py-2 px-4 font-semibold text-white rounded shadow transition duration-150 bg-blue-500 hover:bg-blue-700 active:bg-blue-500 disabled:cursor-not-allowed disabled:bg-gray-500"
             type="submit"
             disabled={isSubmitting}>
             {!isSubmitting && (`Save ${model}`)}
