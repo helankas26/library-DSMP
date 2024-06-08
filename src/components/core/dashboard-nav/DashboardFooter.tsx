3f1a6c38181f85aa1eb8d9e26e0ae10fb8cf7d41
Helanka Singhapurage <helankas26@gmail.com>
2024-06-08 21:09:28 +0530

    add optional color prop to DashboardFooter

--- src/components/core/dashboard-nav/DashboardFooter.tsx
+++ src/components/core/dashboard-nav/DashboardFooter.tsx
@@ -1,17 +1,18 @@
 import React from "react";
 
-const DashboardFooter: React.FC = () => {
+const DashboardFooter: React.FC<{ color?: string }> = (props) => {
+    const {color} = props;
     const currentYear = new Date().getFullYear();
 
     return (
-        <div className="px-4 pb-1 space-y-8 overflow-hidden sm:px-6 lg:px-8 bg-white">
-            <p className=" text-base leading-6 text-center text-[#334155]">
+        <div className={`px-4 pb-1 space-y-8 overflow-hidden sm:px-6 lg:px-8 ${color ? color : 'bg-white'}`}>
+            <p className="text-base leading-6 text-center text-[#334155]">
                 Copyright &copy; {currentYear}
                 &nbsp;
                 <span
                     className="font-bold text-xl text-center bg-clip-text text-transparent bg-gradient-to-r from-rose-600 to-teal-300">
                         Library
-                    </span>
+                </span>
                 &nbsp;
                 by Helanka. All rights reserved.
             </p>
