ce51137df668b30c27f5f73c4dec691fc50a7102
Helanka Singhapurage <helankas26@gmail.com>
2024-08-22 23:24:34 +0530

    bug fix in useUserRole hook bug: current user role does not persist. every time init to undefined; fix: add global var

--- src/hooks/use-user-role.ts
+++ src/hooks/use-user-role.ts
@@ -4,6 +4,8 @@ import {jwtDecode} from "jwt-decode";
 import useAuth from "./use-auth.ts";
 import UserRole from "../enum/UserRole.ts";
 
+let currentUserRole: any = undefined;
+
 const userRoleReducer = (role: string | undefined, action: { type: string; role: string | undefined; }) => {
     if (action.type === 'SET_ROLE') {
         return action.role;
@@ -13,16 +15,19 @@ const userRoleReducer = (role: string | undefined, action: { type: string; role:
 
 const useUserRole = () => {
     const {auth} = useAuth();
-    const [userRole, dispatchUserRole] = useReducer(userRoleReducer, undefined);
-
-    useEffect(() => {
-        const decodeToken = auth.accessToken ? jwtDecode<{ id: string, role: string }>(auth.accessToken) : undefined;
-        const role = decodeToken?.role || undefined;
-        dispatchUserRole({type: 'SET_ROLE', role: role});
-    }, [auth.accessToken]);
+    const [userRole, dispatchUserRole] = useReducer(userRoleReducer, currentUserRole);
 
     const isAdmin = () => userRole === UserRole.Admin;
     const isUser = () => userRole === UserRole.User;
+
+    useEffect(() => {
+        const decodeToken = auth.accessToken ? jwtDecode<{ id: string, role: string }>(auth.accessToken) : undefined;
+
+        if (userRole === undefined || decodeToken?.role !== userRole) {
+            currentUserRole = decodeToken?.role || undefined;
+            dispatchUserRole({type: 'SET_ROLE', role: currentUserRole});
+        }
+    }, [auth.accessToken]);
 
     return {userRole, isAdmin, isUser};
 }
