afefded8c7b8791d8feef7c4ad59bce0fbfe6069
Helanka Singhapurage <helankas26@gmail.com>
2024-09-09 17:07:59 +0530

    change NotFoundError page design

--- src/pages/NotFoundError.tsx
+++ src/pages/NotFoundError.tsx
@@ -1,28 +1,30 @@
 import React from "react";
 import {Link} from "react-router-dom";
 
+import DashboardFooter from "../components/core/dashboard-nav/DashboardFooter.tsx";
+
 const NotFoundError: React.FC = () => {
-    const currentYear = new Date().getFullYear();
 
     return (
         <div className="flex flex-col h-screen">
-            <div className="to-black/10 bg-gradient-to-t from-black/95 via-black/35 overflow-hidden">
+            <div className="bg-[url('assets/home-texture.png')] bg-white overflow-hidden">
                 <div className="w-9/12 m-auto py-16 min-h-screen flex items-center justify-center">
-                    <div className="bg-[url('assets/home-texture.png')] shadow-2xl drop-shadow-2xl overflow-hidden sm:rounded-lg pb-8">
-                        <div className="border-t border-gray-200 text-center pt-8">
-                            <h1 className="text-9xl font-bold text-purple-400">404</h1>
-                            <h1 className="text-6xl font-medium py-8">Oops! Page Not Found</h1>
-                            <p className="text-2xl pb-8 px-12 font-medium">Oops! The page you are looking for does not
-                                exist. It might have been moved or deleted.</p>
+                    <div className="shadow-2xl overflow-hidden sm:rounded-lg pb-8 bg-gray-100">
+                        <div className="border-t border-gray-100 text-center pt-8">
+                            <h1 className="text-9xl font-extrabold text-purple-400">404</h1>
+                            <h1 className="text-5xl font-bold py-8 text-gray-700">Oops! Page Not Found</h1>
+                            <p className="text-2xl pb-8 px-12 font-light text-gray-500">
+                                Oops! The page you are looking for does not exist. It might have been moved or deleted.
+                            </p>
                             <Link to='/' replace={true}
-                                  className="bg-gradient-to-r from-blue-400 to-blue-500 hover:from-blue-500 hover:to-blue-500 text-white font-semibold px-6 py-3 rounded-md mr-6">
+                                  className="bg-sky-600 hover:bg-sky-500 active:bg-sky-600 text-white font-semibold px-6 py-3 rounded-md mr-6">
                                 HOME
                             </Link>
                             <button type="button"
                                     onClick={() => {
                                         window.location.href = 'mailto:helankas26@gmail.com'
                                     }}
-                                    className="bg-gradient-to-r from-red-400 to-red-500 hover:from-red-500 hover:to-red-500 text-white font-semibold px-6 py-3 rounded-md">
+                                    className="bg-red-600 hover:bg-red-500 active:bg-red-600 text-white font-semibold px-6 py-3 rounded-md">
                                 Contact Us
                             </button>
                         </div>
@@ -30,18 +32,7 @@ const NotFoundError: React.FC = () => {
                 </div>
             </div>
             <div className="block">
-                <div className="px-4 pb-1 space-y-8 overflow-hidden sm:px-6 lg:px-8 bg-white}">
-                    <p className="text-base leading-6 text-center text-[#334155]">
-                        Copyright &copy; {currentYear}
-                        &nbsp;
-                        <span
-                            className="font-bold text-xl text-center bg-clip-text text-transparent bg-gradient-to-r from-rose-600 to-teal-300">
-                            Library
-                        </span>
-                        &nbsp;
-                        by Helanka. All rights reserved.
-                    </p>
-                </div>
+                <DashboardFooter color={'bg-[#E2E8F0]'}/>
             </div>
         </div>
     );
