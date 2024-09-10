000c6cb8b576b55bd98a425f182da1a71a7088b8
Helanka Singhapurage <helankas26@gmail.com>
2024-09-10 15:39:09 +0530

    add overflow-x-auto DashboardHeader

--- src/components/core/dashboard-nav/DashboardHeader.tsx
+++ src/components/core/dashboard-nav/DashboardHeader.tsx
@@ -9,29 +9,27 @@ const DashboardHeader: React.FC = () => {
 
     return (
         <div className="bg-[#E2E8F0] mt-2">
-            <div
-                className="mx-1.5 py-2 px-2 bg-white rounded-lg flex items-center justify-between">
+            <div className="mx-1.5 py-2 px-2 bg-white rounded-lg flex items-center justify-between">
                 <div className="px-2 mr-2 border-r-2 border-r-gray-300">
                     <div
                         className="font-bold text-xl text-center bg-clip-text text-transparent bg-gradient-to-r from-rose-600 to-teal-300">
                         Library
                     </div>
                 </div>
-                <div
-                    className="text-gray-700 font-medium capitalize flex flex-1 flex-wrap items-center justify-start gap-2.5">
 
+                <div className="mr-2 rounded text-gray-700 font-medium capitalize flex flex-1 items-center justify-start gap-2.5 overflow-x-auto no-scrollbar">
                     {subRoutes.map((subRoute) => (
                         <NavLink
                             key={subRoute._id}
                             to={subRoute.path}
-                            className={({isActive}) => `${isActive ? `text-orange-700` : undefined} cursor-pointer px-4 py-2 bg-gray-300 hover:text-orange-800 rounded flex items-center`}
+                            className={({isActive}) => `${isActive ? `text-orange-700` : undefined} cursor-pointer px-4 py-2 bg-gray-300 hover:text-orange-800 rounded flex items-center whitespace-nowrap`}
                             end>
 
                             {subRoute.subRoute}
                         </NavLink>
                     ))}
-
                 </div>
+
                 <div className="mr-1">
                     <LoggedMenu/>
                 </div>
